55e67cb1ea78413f75740fe1b51ec2e108335fe3
Daniel <danilagalakov3@gmail.com>
2023-11-22 20:23:20 +0600

    pre-beta: 1.2

--- src/chats/Chat.jsx
+++ src/chats/Chat.jsx
@@ -47,6 +47,7 @@ const Chats = ({dataChats}) => {
     }, []);  
       
     const Send = async () => {
+        setHeightChat(prev => prev = window.visualViewport.height - 130);
         if (!value || value.trim().length < 1) {
             setValue('');
             bottomRef.current.scrollIntoView({ behavior: "smooth", block: "end" })
@@ -65,7 +66,6 @@ const Chats = ({dataChats}) => {
 
         bottomRef.current.scrollIntoView({ behavior: "smooth", block: "end" })
         setValue('');
-        setHeightChat(prev => prev = window.visualViewport.height - 130);
     }
 
     const isURL = (str) => {
